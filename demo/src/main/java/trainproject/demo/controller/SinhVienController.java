24bf400daa51460491458c06b19388bc6e9d1ffb
Lan_Chuot <74306292+lan-code1210@users.noreply.github.com>
2022-07-19 16:39:43 +0700

    commit

---
+++ demo/src/main/java/trainproject/demo/controller/SinhVienController.java
@@ -0,0 +1,12 @@
+package trainproject.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SinhVienController {
+	@GetMapping("sinhvien")
+	public String sinhvien() {
+		return "";
+	}
+}
