2c65aaf83dea57500b003d02ff3569872db61625
Lan_Chuot <74306292+lan-code1210@users.noreply.github.com>
2022-07-19 17:20:19 +0700

    hi

--- demo/src/main/java/trainproject/demo/controller/SinhVienController.java
+++ demo/src/main/java/trainproject/demo/controller/SinhVienController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SinhVienController {
-	@GetMapping("sinhvien")
+	@GetMapping("giangvien")
 	public String sinhvien() {
 		return "";
 	}
+	
 }
